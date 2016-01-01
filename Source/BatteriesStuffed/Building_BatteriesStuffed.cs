6cc33f60fb9d091292b6a638953dc44e248278b2
Skullywag <mjskull@gmail.com>
2016-01-01 09:20:50 +0000

    Made batteries minifiable, refactored classes

--- Source/BatteriesStuffed/Building_BatteriesStuffed.cs
+++ Source/BatteriesStuffed/Building_BatteriesStuffed.cs
@@ -7,12 +7,22 @@ namespace BatteriesStuffed
 {
     public class Building_StuffedBattery : Building
     {
-        private static readonly Vector2 BarSize = new Vector2(1.3f, 0.4f);
-        private static readonly Material BarFilledMat = SolidColorMaterials.SimpleSolidColorMaterial(new Color(0.9f, 0.85f, 0.2f));
-        private static readonly Material BarUnfilledMat = SolidColorMaterials.SimpleSolidColorMaterial(new Color(0.3f, 0.3f, 0.3f));
+        private const float MinEnergyToExplode = 500f;
+		private const float EnergyToLoseWhenExplode = 400f;
+		private const float ExplodeChancePerDamage = 0.05f;
+		private int ticksToExplode;
+		private Sustainer wickSustainer;
+		private static readonly Vector2 BarSize = new Vector2(1.3f, 0.4f);
+		private static readonly Material BarFilledMat = SolidColorMaterials.SimpleSolidColorMaterial(new Color(0.9f, 0.85f, 0.2f));
+		private static readonly Material BarUnfilledMat = SolidColorMaterials.SimpleSolidColorMaterial(new Color(0.3f, 0.3f, 0.3f));
+		private static readonly SoundDef WickSound = SoundDef.Named("HissSmall");
+        private bool hasRand = false;
+        private int chance;
+        ThingDef moteThrown = ThingDef.Named("Mote_BlastEMP");
         public override void ExposeData()
         {
             base.ExposeData();
+            Scribe_Values.LookValue<int>(ref this.ticksToExplode, "ticksToExplode", 0, false);
         }
         public override void Draw()
         {
@@ -28,11 +38,40 @@ namespace BatteriesStuffed
             Rot4 rotation = base.Rotation;
             rotation.Rotate(RotationDirection.Clockwise);
             r.rotation = rotation;
-            GenDraw.DrawFillableBar(r);           
+            GenDraw.DrawFillableBar(r);
+            if (this.ticksToExplode > 0)
+            {
+                OverlayDrawer.DrawOverlay(this, OverlayTypes.BurningWick);
+            }
         }
         public override void Tick()
         {
-            base.Tick();            
+            base.Tick();
+            if (this.ticksToExplode > 0)
+            {
+                this.wickSustainer.Maintain();
+                this.ticksToExplode--;
+                if (this.ticksToExplode == 0)
+                {
+                    IntVec3 randomCell = this.OccupiedRect().RandomCell;
+                    float radius = Rand.Range(0.5f, 1f) * 3f;
+                    GenExplosion.DoExplosion(randomCell, radius, DamageDefOf.Flame, null, null, null, 1f);
+                    base.GetComp<CompPowerBattery>().DrawPower(400f);
+                }
+            }         
+        }
+
+        public override void PostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
+        {
+            if (this.def.defName != "PlasteelBattery" || this.def.defName != "UraniumBattery")
+            {
+                if (!base.Destroyed && this.ticksToExplode == 0 && dinfo.Def == DamageDefOf.Flame && Rand.Value < 0.05f && base.GetComp<CompPowerBattery>().StoredEnergy > 500f)
+                {
+                    this.ticksToExplode = Rand.Range(70, 150);
+                    SoundInfo info = SoundInfo.InWorld(this, MaintenanceType.PerTick);
+                    this.wickSustainer = Building_StuffedBattery.WickSound.TrySpawnSustainer(info);
+                }
+            }
         }
     }
 }
